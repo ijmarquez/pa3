a45f8ad077055231cebb6844c0bbf680f6514df7
cpoon4 <poonch@uci.edu>
2017-05-24 23:26:41 -0700

    Shopping cart template

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -15,25 +15,32 @@ public class ShoppingCart extends HttpServlet {
         Constants.header(out);
 
         //body]
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
-        out.println("");
+        out.println("<div class=\"shoppingContainer\">");
+        out.println("<h2> Shopping Cart</h2> <br><br>");
+        out.println("<div>");
+        out.println("<table style=\"margin-left: auto; margin-right: auto\">");
+        //dynamically add here
+        out.println("<tr>");
+        out.println("<td><p> item 1</p></td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<td><p>Size: </p></td>");
+        out.println(" <td><p>Quantity: <input type=\"text\" name=\"quantity\" size=\"3\" value=1 class=\"inputReadOnly\" readonly> </p></td>");
+        out.println("<td><p>Cost: $<input name=\"unitPrice\" class=\"inputReadOnly\" readonly> </input></p></td>");
+        out.println("</tr>");
+        //end of each item
+        out.println("</table>");
+        out.println("</div>");
+        out.println("<br><hr>");
+        out.println("<div class=\"centerOverview\">");
+        out.println("<p>Tax rate: $ <input id=\"taxPicked\" name=\"tax\" value=\"0\" onkeyup=\"updateTotal()\" class=\"inputReadOnly\" readonly> </input></p>");
+        out.println("<p>Total Tax: $<input id=\"taxTotal\" name= 'taxTotal' class=\"inputReadOnly\" value=\"0\" readonly> </input></p>");
+        out.println("<p>Total: $ <input input id=\"totalCost\" name=\"total\" value=\"20.00\" class=\"inputReadOnly\" readonly> </input></p>");
+        out.println("</div>");
+        out.println("<div id=\"submitOrder\">");
+        out.println("<input id=\"btn\" type=\"submit\" value=\"Checkout\" onclick=\"href='BuyItem'\">");
+        out.println("</div>");
+        out.println("</div>");
 
         //footer
         Constants.footer(out);
